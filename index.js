488af95022b6c1215da047145f2bdb42e3dadfe2
tacticalParmesan <edoardoferreri10@gmail.com>
2024-01-28 14:55:14 +0100

    Added a functiont to adjust the size of the squares dynamically

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function createGrid(rows=16, cols= 16) {
   // Creating a webpage with a 16*16 grid of square divs;
   const gridContainer = document.querySelector(".grid-container");
+  const gridSize = getComputedStyle(gridContainer).width.replace("px", "");
+
 
   for(let i = 0; i <= rows; i++) {
 
@@ -12,11 +14,20 @@ function createGrid(rows=16, cols= 16) {
     for (let j = 0; j <= cols; j++) {
       const newSquare = document.createElement("div");
       newSquare.classList.add("square")
+      
+      // Adjust the square size dynamically 
+      squareSize = adjustSquaresToGrid(gridSize, cols, "px");
+      newSquare.style.width = newSquare.style.height = squareSize;
       newRow.appendChild(newSquare);
     }
     gridContainer.appendChild(newRow);
   }
 }
 
+function adjustSquaresToGrid(gridSize, squares, unit) {
+  // To obtain a dynamic grid we need to make the squares size proportional to the container;
+  const squareSize = (Math.round((gridSize / squares) * 10) / 10).toFixed(1);
+  return squareSize + unit;
+}
 
-createGrid();
+createGrid(100, 100);
