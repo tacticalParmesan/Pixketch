ec4df40ac1b5379ed7bc0298b8e1ba6a7fbd9a96
tacticalParmesan <edoardoferreri10@gmail.com>
2024-01-28 13:24:09 +0100

    Created the function to create a grid of squares

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-// Creating a webpage with a 16*16 grid of square divs;
+function createGrid(rows=16, cols= 16) {
+  // Creating a webpage with a 16*16 grid of square divs;
+  const gridContainer = document.querySelector(".grid-container");
 
-const gridContainer = document.querySelector(".grid-container");
+  for(let i = 0; i <= rows; i++) {
+
+    // Create a new row and append it to the grid...
+    const newRow = document.createElement("div");
+    newRow.classList.add("grid-row");
+    
+    //...then create n squares for each column and append each one to the row 
+    for (let j = 0; j <= cols; j++) {
+      const newSquare = document.createElement("div");
+      newSquare.classList.add("square")
+      newRow.appendChild(newSquare);
+    }
+    gridContainer.appendChild(newRow);
+  }
+}
+
+
+createGrid();
